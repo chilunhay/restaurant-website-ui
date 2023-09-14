7e3040d4a0f1993e061944f907e49489f2dceadd
chilunhay <72005042+chilunhay@users.noreply.github.com>
2023-09-14 09:32:18 +0700

    fix some error in Menu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -19,24 +19,15 @@ const Menu = () => {
   const user = false;
   return (
     <div>
-      {!open ? (
-        <Image
-          src="/open.png"
-          alt="open"
-          width={20}
-          height={20}
-          onClick={() => setOpen(true)}
-        />
-      ) : (
-        <Image
-          src="/close.png"
-          alt="close"
-          width={20}
-          height={20}
-          onClick={() => setOpen(false)}
-        />
-      )}
-
+      {/* SHORTCUT */}
+      <Image
+        src={open ? "/close.png" : "/open.png"}
+        alt=""
+        width={20}
+        height={20}
+        onClick={() => setOpen(!open)}
+        className="cursor-pointer"
+      />
       {open && (
         <div className="bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10">
           {links.map((item) => (
@@ -44,15 +35,13 @@ const Menu = () => {
               {item.title}
             </Link>
           ))}
-          {!user ? (
-            <Link href="/login" onClick={() => setOpen(false)}>
-              Login
-            </Link>
-          ) : (
-            <Link href="/orders" onClick={() => setOpen(false)}>
-              Orders
-            </Link>
-          )}
+          {/* SHORTCUT */}
+          <Link
+            href={user ? "/orders" : "login"}
+            onClick={() => setOpen(false)}
+          >
+            {user ? "Orders" : "Login"}
+          </Link>
           <Link href="/cart" onClick={() => setOpen(false)}>
             <CartIcon />
           </Link>
